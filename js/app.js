51afcc3b607c9927ae5bc0174a850aef93817d88
DAVID TUSIA <DAVE.TUSIA@GMAIL.COM>
2022-07-25 23:51:28 -0700

    re-done, new commit with assets

--- js/app.js
+++ js/app.js
@@ -3,13 +3,25 @@
 
 console.log('Hi');
 
-// Total rounds before code is haulted
+let myContainer = document.querySelector('section');
+let myButton = document.querySelector('section + div');
+
+let ul = document.querySelector('ul');
+
+let image1 = document.querySelector('section img:first-child');
+let image2 = document.querySelector('section img:nth-child(2)');
+let image3 = document.querySelector('section img:nth-child(3)');
+
+
 let totalRounds = 25;
 
-// Conductor representing the ducks
-function Ducks(name, image) {
+let allDucks = [];
+let clicks = 0;
+let clicksAllowed = 3;
+
+function Ducks(name, fileExtension = 'jpg') {
   this.name = name;
-  this.image = image;
+  this.src = `assets/${this.name}.${fileExtension}`;
   this.views = 0;
   this.clicks = 0;
 }
@@ -17,22 +29,6 @@ function Ducks(name, image) {
 function getRandomDuck() {
   return Math.floor(Math.random() * allDucks.length);
 }
-
-// Array holding the created ducks
-let allDucks = [];
-console.log(allDucks);
-
-// Ducks to be created
-let duck1 = new Ducks('duck1', 'jpg1');
-let duck2 = new Ducks('duck2', 'jpg2');
-let duck3 = new Ducks('duck3', 'jpg3');
-let duck4 = new Ducks('duck4', 'jpg4');
-let duck5 = new Ducks('duck5', 'jpg5');
-let duck6 = new Ducks('duck6', 'jpg6');
-let duck7 = new Ducks('duck7', 'jpg7');
-
-allDucks.push(duck1, duck2, duck3, duck4, duck5, duck6, duck7);
-console.log(allDucks);
 
 function renderDucks() {
   let d1 = getRandomDuck();
@@ -44,23 +40,72 @@ function renderDucks() {
       d2 = getRandomDuck();
     }
   }
-  console.log(d1, d2, d3);
+  image1.src = allDucks[d1].src;
+  image1.src = allDucks[d1].name;
+  image2.src = allDucks[d2].src;
+  image2.src = allDucks[d2].name;
+  image3.src = allDucks[d3].src;
+  image3.src = allDucks[d3].name;
+  allDucks[d1].views++;
+  allDucks[d2].views++;
+  allDucks[d3].views++;
 }
+
+function handleDuckClick(event) {
+  if (event.target === myContainer) {
+    alert('Please click on an image');
+  }
+  clicks++
+  let clickedDuck = event.target.alt;
+  console.log(clickedDuck);
+  for (let i = 0; i < allGoats.length; i++) {
+    if (clickedDuck === allDucks[i].name) {
+      allDucks[i].clicks++;
+      break;
+    }
+  }
+  renderDucks();
+  if (clicks === clicksAllowes) {
+    myButton.className = 'clicks-allowed';
+    myContainer.removeEventListener('click', handleDuckClick);
+    myButton.addEventListener('click', handleButtonClick);
+  }
+}
+
+function handleButtonClick() {
+    renderResults();
+}
+
+function renderResults() {
+  for (let i = 0; i < allDucks.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = `${allDucks[i].name} had ${allDucks[i].views} views and was clicked on ${allDucks[i].clicks} times`;
+    ul.appendChild(li);
+  }
+}
+
+let duck1 = new Ducks('bag');
+let duck2 = new Ducks('banana');
+let duck3 = new Ducks('bathroom');
+let duck4 = new Ducks('boots');
+let duck5 = new Ducks('breakfast');
+let duck6 = new Ducks('bubblegum');
+let duck7 = new Ducks('chair');
+let duck8 = new Ducks('cthulhu');
+let duck9 = new Ducks('dog-duck');
+let duck10 = new Ducks('dragon');
+let duck11 = new Ducks('pen');
+let duck12 = new Ducks('pet-sweep');
+let duck13 = new Ducks('scissors');
+let duck14 = new Ducks('shark');
+let duck15 = new Ducks('sweep', 'png');
+let duck16 = new Ducks('tauntaun');
+let duck17 = new Ducks('unicorn');
+let duck18 = new Ducks('water-can');
+let duck19 = new Ducks('wine-glass');
+
+allDucks.push(duck1, duck2, duck3, duck4, duck5, duck6, duck8, duck9, duck10, duck11, duck12, duck13, duck14, duck15, duck16, duck17, duck18, duck19);
+
 renderDucks();
 
-
-
-
-
-
-// function renderResults() {
-//   let ul = document.querySelector('ul');
-//   for (let i = 0; i < allDucks.length; i++) {
-//     let li = document.childElement('li');
-//     li.textContent = ${allDcuks[i].name }
-//   }
-// }
-
-// function renderDucks() {
-//   let duck1 = getRandomDuck
-// }
+myContainer.addEventListener('click', handleDuckClick);
